a72141be7437cff988f395bb08b7ccd92c2bfdfb
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-01 12:23:29 +0200

    Controller done.

--- Presentation/Controllers/SchedulesController.cs
+++ Presentation/Controllers/SchedulesController.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Domain.Forms;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class SchedulesController : ControllerBase
+public class SchedulesController(IScheduleService scheduleService) : ControllerBase
 {
+    private readonly IScheduleService _scheduleService = scheduleService;
 
+    [HttpPost]
+    public async Task<IActionResult> Create(AddScheduleForm addForm)
+    {
+        if (!ModelState.IsValid) { return BadRequest(); }
 
+        var result = await _scheduleService.AddScheduleAsync(addForm);
+        return result.Success ? Ok(result) : BadRequest(result);
+    }
 
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateScheduleForm updateForm)
+    {
+        if (!ModelState.IsValid) { return BadRequest(); }
 
+        var result = await _scheduleService.UpdateScheduleAsync(updateForm);
+        return result.Success ? Ok(result) : BadRequest(result);
+    }
 
+    [HttpGet]
+    public async Task<IActionResult> Get(string id)
+    {
+        if (id == null) { return BadRequest(); }
 
+        var result = await _scheduleService.GetScheduleAsync(id);
+        return result.Success ? Ok(result) : BadRequest(result);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Delete(string id)
+    {
+        if (id == null) { return BadRequest(); }
 
-
-
-
-
+        var result = await _scheduleService.DeleteScheduleAsync(id);
+        return result.Success ? Ok(result) : BadRequest(result);
+    }
 }
