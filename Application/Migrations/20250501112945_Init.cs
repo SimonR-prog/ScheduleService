08f5d32b5f7b493a3d67eb1d769d8795a66ee501
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-01 13:33:16 +0200

    Migrated, updated and added gitignore on appsettings.

---
+++ Application/Migrations/20250501112945_Init.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    EventId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GateOpenStart = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GateOpenEnd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PreShowStart = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PreShowEnd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CeremonyStart = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CeremonyEnd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcertStart = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.EventId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
